2137e78da0f27aa36f6d01df02b8af56ff03c261
Vandal Paulius Juska <aki.maxis@gmail.com>
2018-08-13 21:37:12 +0100

    Added about page content

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -12,6 +12,30 @@ class App extends React.Component {
         this.state = {
             config: {
                 websiteName: 'Robert Photo',
+                aboutMe: [{
+                    id: 'asdasd',
+                    image: true,
+                    alt: 'Fabulous Rob',
+                    url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/26805064_10215337302006544_272682501393385032_n.jpg?_nc_cat=0&oh=787c47aa5cfa28f1b779fb828030c984&oe=5C02BFE3',
+                }, {
+                    id: 'ds4fsd6f4sd',
+                    text: true,
+                    heading: true,
+                    content: 'Lorem Ipsum',
+                }, {
+                    id: '6ds5fsdf56ad',
+                    text: true,
+                    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+                }, {
+                    id: '56dsf4sd6f465484dsf565',
+                    image: true,
+                    alt: 'More of him',
+                    url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/17156214_10212262683222996_3831772522503633545_n.jpg?_nc_cat=0&oh=8de33e8b224c176a85f96298bddbc35e&oe=5C00B928',
+                }, {
+                    id: 'sdfsd989223a',
+                    text: true,
+                    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+                }],
             },
             menuOpen: false,
         };
@@ -49,7 +73,14 @@ class App extends React.Component {
                         </div>
                         <div className={styles.routes}>
                             <Route exact path="/" component={Gallery} />
-                            <Route path="/about" component={About} />
+                            <Route
+                                path="/about"
+                                component={() => (
+                                    <About
+                                        description={this.state.config.aboutMe}
+                                    />
+                                )}
+                            />
                             <Route path="/contact" component={Contact} />
                         </div>
                     </div>
--- src/app/views/about/About.jsx
+++ src/app/views/about/About.jsx
@@ -1,20 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './styles.scss';
 
-function About() {
+function About(props) {
     return (
-        <div>
-            About
+        <div className={styles.about}>
+            {props.description.map((data, index) => {
+                if (data.image) {
+                    return (
+                        <div key={data.id} className={styles.imageContainer}>
+                            <img src={data.url} alt={data.alt} className={styles.image}/>
+                        </div>
+                    );
+                }
+
+                if (data.text) {
+                    let textAfter = false;
+
+                    if (data.heading) {
+                        if (index + 1 < props.description.length) {
+                            const upcoming = props.description[index + 1];
+                            if (upcoming.text && !upcoming.heading) {
+                                textAfter = true;
+                            }
+                        }
+                    }
+
+                    return (
+                        <div
+                            key={data.id}
+                            className={
+                                `${
+                                    styles.text
+                                } ${
+                                    data.heading ? styles.heading : ''
+                                } ${
+                                    textAfter ? styles.textAfter : ''
+                                }`
+                            }
+                        >
+                            {data.content}
+                        </div>
+                    );
+                }
+            })}
         </div>
     );
 }
 
 About.propTypes = {
-
+    description: PropTypes.arrayOf(PropTypes.shape({})),
 };
 
 About.defaultProps = {
-
+    description: [],
 };
 
 export default About;
